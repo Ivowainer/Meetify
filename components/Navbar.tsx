d720acefe1ff42ed80ecab7e5cd8b45184d57e56
Ivowainer <ivancamposwainer@gmail.com>
2024-03-29 14:42:15 -0300

    DarkColors; Sidebar n Navbar (mobile n desktop)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import MobileNav from "./MobileNav";
+
 const Navbar = () => {
     return (
-        <div className=""></div>
+        <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+            <Link href="/" className="flex items-center gap-1">
+                <Image 
+                    src="/icons/logo.svg"
+                    width={32}
+                    height={32}
+                    alt="Logo Meetify Navbar"
+                    className="max-sm:size-10"
+                />
+                <p className="text-[26px] font-extrabold text-white max-sm:hidden">Meetify</p>
+            </Link>
+
+            <div className="flex-between gap-5">
+                {/* Clerk - User Managment */}
+
+                <MobileNav />
+
+            </div>
+        </nav>
     );
 };
 
